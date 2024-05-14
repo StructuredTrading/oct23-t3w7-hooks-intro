637fd5b538a2c8198cd28178f968f0d89edf28e1
StructuredTrading <samowen1992@hotmail.com>
2024-05-14 20:24:33 +1000

    feat: fetching pokemon data to display

--- src/App_FunctionComponent.js
+++ src/App_FunctionComponent.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 
   const [message, setMessage] = useState("Hello world");
-  const [state2, setState2] = useState("e");
+  const [pokemon, setPokemon] = useState({name: "", sprite: ""});
 
 
   // useEffect with empty dependency array = 
@@ -14,6 +14,14 @@ function App() {
     console.log("Component loaded");
   }, []);
 
+  // useEffect with return function = 
+  // gonna call that function on componentWillUnmount
+  // not related to the dependency array at all 
+  useEffect(() => {
+    return (() => {
+      console.log("Component will unmount");
+    });
+  }, []);
 
   // useEffect with variable in dependency array = 
   // depends on variable, will run when declared AND updated 
@@ -23,24 +31,32 @@ function App() {
   }, [message]);
 
 
-  // useEffect with return function = 
-  // gonna call that function on componentWillUnmount
-  // not related to the dependency array at all 
+  const getPokemon = async () => {
+    let result = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1025) + 1}`).then(response => response.json());
+    setPokemon({name: result.name, sprite: result.sprites.front_default});
+  }
+
   useEffect(() => {
-    return (() => {
-      console.log("component will unmount");
-    })
-  })
+    getPokemon();
+  }, []);
+
 
   return (
     <div className="App">
       <h1>{message}</h1>
-      {/* <h1>{message}{state2}</h1> */}
-      <button onClick={() => setMessage('New message')}>
+      {/* <h1>{state}{state2}</h1> */}
+      <button onClick={() => setMessage(321654658465846)}>
         Change message
       </button>
 
       <input type='text' value={message} onChange={(event) => setMessage(event.target.value)} />
+
+
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.sprite} alt='URL to the sprite of whatever random Pokemon this is.' />
+      <button onClick={getPokemon}>
+        Get random Pokemon 
+      </button>
     </div>
   );
 }
